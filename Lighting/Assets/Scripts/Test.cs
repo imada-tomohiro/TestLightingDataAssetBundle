1b8fcd33cd0943d3fcd6011b323875649b2a05f7
imada-tomohiro <imada.tomohiro@donuts.ne.jp>
2016-10-27 18:28:16 +0900

    通常のサブシーン読み込みに修正

--- Lighting/Assets/Scripts/Test.cs
+++ Lighting/Assets/Scripts/Test.cs
@@ -7,40 +7,6 @@ public class Test : MonoBehaviour {
   
   public void OnClickAssetBundleLoad()
   {
-    StartCoroutine(LoadAssetBundle());
-  }
-
-
-  IEnumerator LoadAssetBundle()
-  {
-    
-    Debug.Log("file://" + Application.streamingAssetsPath + "/Android/test/light");
-    
-    WWW lightwww = new WWW("file://" + Application.streamingAssetsPath + "/Android/test/light");
-    
-    yield return lightwww;
-    
-    AssetBundle bundle = lightwww.assetBundle;
-    
-    Object[] objects = bundle.LoadAllAssets();
-    
-    foreach (Object obj in objects)
-    {
-      Debug.Log("obj=" + obj.name);
-      Debug.Log("objtype=" + obj.GetType().ToString());
-    }
-
-    publicObjects = objects;
-    Debug.Log("Light");
-
-    Debug.Log("file://" + Application.streamingAssetsPath + "/Android/test/scene");
-
-    WWW scenewww = new WWW(Application.streamingAssetsPath +  "/Android/test/scene");
-
-    yield return scenewww;
-
-    AssetBundle scenebundle = scenewww.assetBundle;
-
     Debug.Log("LoadScene");
     SceneManager.LoadSceneAsync("LightingScene", LoadSceneMode.Additive);
   }
